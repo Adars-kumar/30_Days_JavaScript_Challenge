5ada766c097054c55630d0604d16fb54ff5fee5a
Adarsh <106640566+Adars-kumar@users.noreply.github.com>
2024-07-18 15:20:10 +0530

    Add files via upload

---
+++ Day1_VaraiblesAndDataTypes/script.js
@@ -0,0 +1,43 @@
+// Activity - 1 Variable Decalaration 
+// ----- Task - 1 Declare a varaible using var, assign it a number and log the value to the console.
+var num = 5;
+console.log("Number = " + num);
+
+// ----- Task - 2 Declare a varaible using let, assign it a string and log the value to the console.
+let str = "30 days of Javascript challenge accepted"
+console.log("String = " + str);
+
+// Activity - 2 Constant Decalaration 
+// ----- Task - 3 Declare a varaible using const, assign it a boolean value and log the value to the console.
+const value = true;
+console.log("Boolean value = " + value);
+
+// Activity - 3 Data Types
+// ----- Task - 4 Create varaibles of different data types(number , string, boolean , objext , array) and log each variables's type using typeof operator.
+var num1 = 5;
+let str1 = " javascript"
+const value1 = false
+console.log(typeof num1);
+console.log(typeof str1);
+console.log(typeof value1);
+
+// Activity - 4 Reassigning Variables
+// ----- Task - 5 Declare a variable using let, assign it an inital value, reassign a new balue and log both value to the console.
+let num2 = 6;
+console.log(num2);
+num2 = 45;
+console.log(num2);
+
+// Activity - 5  Understanding const
+// ----- Task - 6  Try assigning a variable declared with const and observe the error.
+const a = 35;
+console.log(a);
+a = 45;
+console.log(a);
+ 
+// Explanation : The const keyword is used to create a constant variable,
+//  which means that once it is assigned a value, it cannot be reassigned. 
+
+
+
+
